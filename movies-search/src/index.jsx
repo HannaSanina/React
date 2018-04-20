84f60828811ef40b73d28cab4f63a48c50e9caf9
Hanna Sanina <hanna.sanina@thomsonreuters.com>
2018-04-20 16:29:18 +0300

    Configured Webpack for dev and prod mode. Added test task

--- movies-search/src/index.jsx
+++ movies-search/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import _ from 'lodash';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 const Title = React.createElement('h1', {}, 'The greatest movie collection');
---
+++ movies-search/webpack.prod.js
@@ -0,0 +1,19 @@
+const webpack = require("webpack");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = merge(common, {
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
+})
