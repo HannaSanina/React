b3595145540232f883ad9e9e8305f4a64b5d832e
Hanna Sanina <hanna.sanina@thomsonreuters.com>
2018-05-03 16:42:10 +0300

    Add code improvements proposed by the mentor

--- movies-search/src/Search.jsx
+++ movies-search/src/Search.jsx
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom';
 import styles from './styles/App.css'
 
 class Search extends React.PureComponent {
-    render() {
-      return <div><div>Find your movie </div>
-        <input className={styles.search} placeholder="Type here.."></input>
-        <div className={styles.filter}>
-          <span>Search by <span><button>Title</button><button>Director</button> </span></span>
-          <button>Search</button>
-        </div>
-      </div>;
-    }
+  selectedFilter = "Title";
+  getStyle = (name) => {
+    return name == this.selectedFilter ? { "backgroundColor":"cornflowerblue" } :  { "backgroundColor":"aliceblue" };
+  };
+
+  render() {
+    return <div><div>Find your movie </div>
+      <input className={styles.search} placeholder="Type here..">{this.props.search}</input>
+      <div className={styles.filter}>
+        <span>Search by
+          <span>
+            <button style = {this.getStyle(filters[0])}>{filters[0]}</button>
+            <button style = {this.getStyle(filters[1])}>{filters[1]}</button>
+          </span>
+        </span>
+        <button>Search</button>
+      </div>
+    </div>;
   }
- export default Search;
+}
+export default Search;
+
+const filters = ["Title", "Director"]
